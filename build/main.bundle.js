5a2a688832be90a9a84abd2feba453125cf5a42a
Mauricio <thiswallz@gmail.com>
2018-08-19 17:51:23 +0200

    chore: add build process

---
+++ build/main.bundle.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var UPPER_VELOCITY = 10;
+var LOWER_VELOCITY = 2;
+var MAX_SIZE = innerWidth * .05;
+var MIN_SIZE = innerWidth * .025;
+var MAX_PARTICLES = 1000;
+var AMOUNT = 0;
+var COLOR = 0xffffff;
+var GRAPHIC_SIZE = 500;
---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+const UPPER_VELOCITY = 10
+const LOWER_VELOCITY = 2
+const MAX_SIZE = innerWidth * .05
+const MIN_SIZE = innerWidth * .025
+const MAX_PARTICLES = 1000
+const AMOUNT = 0
+const COLOR = 0xffffff
+const GRAPHIC_SIZE = 500
+
