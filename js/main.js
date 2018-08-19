a1f686a42437c473a5643244e7d64990b393d93d
Mauricio <thiswallz@gmail.com>
2018-08-19 19:21:38 +0200

    feat: add das lineal

--- js/main.js
+++ js/main.js
@@ -1,14 +1,32 @@
 import * as PIXI from 'pixi.js';
 
-//Create a Pixi Application
-let app = new PIXI.Application({width: 256, height: 256});
-
-//Add the canvas that Pixi automatically created for you to the HTML document
+var app = new PIXI.Application(800, 600, {backgroundColor: 0x1099bb});
 document.body.appendChild(app.view);
 
-let type = 'WebGL';
-if (!PIXI.utils.isWebGLSupported()) {
-  type = 'canvas';
-}
+// create a new Sprite from an image path
+var texture = PIXI.Texture.fromImage(
+  'public/assets/das.lineal.svg',
+  //	'https://s3-us-west-2.amazonaws.com/s.cdpn.io/106114/bee.svg',
+  undefined,
+  undefined,
+  1.0,
+);
 
-PIXI.utils.sayHello(type);
+var bunny = new PIXI.Sprite(texture);
+
+// center the sprite's anchor point
+bunny.anchor.set(0.5);
+
+// move the sprite to the center of the screen
+bunny.x = app.screen.width / 2;
+bunny.y = app.screen.height / 2;
+
+app.stage.addChild(bunny);
+
+// Listen for animate update
+app.ticker.add(function(delta) {
+  // just for fun, let's rotate mr rabbit a little
+  // delta is 1 if running at 100% performance
+  // creates frame-independent transformation
+  bunny.rotation += 0.1 * delta;
+});
